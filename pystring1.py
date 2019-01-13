f712cb540d53cb1b621c06c7cfc0ed91d63c16be
tubreddy <tubreddy@gmail.com>
2019-01-13 11:44:40 -0600

    My First

---
+++ pystring1.py
@@ -0,0 +1,11 @@
+name = "John"
+print("Hello, %s!" % name)
+
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age))
+
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
